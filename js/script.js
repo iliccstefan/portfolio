095a546e23c3a71ec9f07ceb12bc4afce3bae561
Stefan <stefan.ilic50@gmail.com>
2025-04-03 23:00:04 +0200

    refactor project section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenu.classList.toggle('active');
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});

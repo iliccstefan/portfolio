b296e5433408001dda4f4f73bcb9993f18e9bf37
Stefan <stefan.ilic50@gmail.com>
2025-04-07 23:41:28 +0200

    fix hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.mobile-display');
+    const mobileMenu = document.querySelector('nav.mobile-display');
+
     toggleButton.addEventListener('click', function () {
         mobileMenu.classList.toggle('active');
     });

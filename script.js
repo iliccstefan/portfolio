34e13d0e7b0572f7f3a07a53f7c2e5e36c92342e
Stefan <stefan.ilic50@gmail.com>
2025-04-25 18:54:59 +0200

    impruve projects and contact section on small screens

--- script.js
+++ script.js
@@ -17,13 +17,14 @@ document.addEventListener("click", (event) => {
 });
 
 // Hambuerger menu click/toggle effect
-
 document.addEventListener("DOMContentLoaded", () => {
 	const hamburgerIcon = document.querySelector(".mobile-navbar__hamburger");
-	const mobileNavbarMenu = document.querySelector(".mobile-navbar__menu");
+	const mobileNavbarMenu = document.querySelector(
+		".mobile-navbar__menu-container"
+	);
 
 	hamburgerIcon.addEventListener("click", () => {
-		mobileNavbarMenu.classList.toggle("hidden");
+		/* mobileNavbarMenu.classList.toggle("hidden"); */
 		mobileNavbarMenu.classList.toggle("active");
 	});
 
@@ -37,3 +38,18 @@ document.addEventListener("DOMContentLoaded", () => {
 		form.reset();
 	});
 });
+
+// My current age
+const myBirthDate = new Date("1995-06-28");
+const today = new Date();
+
+const age = today.getFullYear() - myBirthDate.getFullYear();
+
+const hasBirthdayPassed =
+	today.getMonth() < myBirthDate.getMonth() ||
+	(today.getMonth() === myBirthDate.getMonth() &&
+		today.getDate() < myBirthDate.getDate());
+
+const myCurrentAge = document.querySelector(".my-current-age");
+
+myCurrentAge.textContent = Boolean(hasBirthdayPassed) ? age - 1 : age;

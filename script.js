893debfd67aca7fb23c31f58514659a652bf7d17
Stefan <stefan.ilic50@gmail.com>
2025-05-20 21:37:00 +0200

    update contact form functionality

--- script.js
+++ script.js
@@ -1,55 +1,68 @@
 // Curent year in footer
-const currentYear = document.querySelector(".current-year");
+const currentYear = document.querySelector('.current-year');
 
 currentYear.textContent = new Date().getFullYear();
 
 // Anchor link smooth scroll
-document.addEventListener("click", (event) => {
-	const { target } = event;
+document.addEventListener('click', (event) => {
+    const { target } = event;
 
-	if (!target.matches('a[href^="#"]')) return;
+    if (!target.matches('a[href^="#"]')) return;
 
-	event.preventDefault();
+    event.preventDefault();
 
-	document
-		.querySelector(target.getAttribute("href"))
-		.scrollIntoView({ behavior: "smooth" });
+    document.querySelector(target.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
 });
 
 // Hambuerger menu click/toggle effect
-document.addEventListener("DOMContentLoaded", () => {
-	const hamburgerIcon = document.querySelector(".mobile-navbar__hamburger");
-	const mobileNavbarMenu = document.querySelector(
-		".mobile-navbar__menu-container"
-	);
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerIcon = document.querySelector('.mobile-navbar__hamburger');
+    const mobileNavbarMenu = document.querySelector('.mobile-navbar__menu-container');
 
-	hamburgerIcon.addEventListener("click", () => {
-		/* mobileNavbarMenu.classList.toggle("hidden"); */
-		mobileNavbarMenu.classList.toggle("active");
-	});
+    hamburgerIcon.addEventListener('click', () => {
+        /* mobileNavbarMenu.classList.toggle("hidden"); */
+        mobileNavbarMenu.classList.toggle('active');
+    });
 
-	const formSubmitButton = document.querySelector(
-		".contact-section__submit-button"
-	);
+    const form = document.querySelector('form');
 
-	formSubmitButton.addEventListener("submit", () => {
-		const form = document.querySelector(".contact-section__form");
+    form.addEventListener('submit', async (event) => {
+        try {
+            event.preventDefault();
 
-		form.reset();
-	});
+            const name = document.querySelector('.contact-section__name-field').value.trim();
+            const email = document.querySelector('.contact-section__email-field').value.trim();
+            const message = document.querySelector('.contact-section__message-field').value.trim();
+            const form = document.querySelector('.contact-section__form');
+            const url = 'https://api.stefanilic.dev/api/v1/send-email';
+            const method = 'POST';
+            console.log(name, email, message);
+            const body = JSON.stringify({ name, email, message });
+            const response = await fetch(url, { method, 'Content-Type': 'application/json', body });
+
+            const { error, responseMessage } = await response.json();
+
+            if (error) {
+                return;
+            }
+
+            form.reset();
+        } catch (error) {
+            console.error(error);
+        }
+    });
 });
 
 // My current age
-const myBirthDate = new Date("1995-06-28");
+const myBirthDate = new Date('1995-06-28');
 const today = new Date();
 
 const age = today.getFullYear() - myBirthDate.getFullYear();
 
 const hasBirthdayPassed =
-	today.getMonth() < myBirthDate.getMonth() ||
-	(today.getMonth() === myBirthDate.getMonth() &&
-		today.getDate() < myBirthDate.getDate());
+    today.getMonth() < myBirthDate.getMonth() ||
+    (today.getMonth() === myBirthDate.getMonth() && today.getDate() < myBirthDate.getDate());
 
-const myCurrentAge = document.querySelector(".my-current-age");
+const myCurrentAge = document.querySelector('.my-current-age');
 
 myCurrentAge.textContent = Boolean(hasBirthdayPassed) ? age - 1 : age;
